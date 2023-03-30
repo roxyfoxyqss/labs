9fcdfcca10ad03167aab85e4d51adab80edeef44
roxyfoxyqss <67821994+roxyfoxyqss@users.noreply.github.com>
2023-03-30 21:09:50 +0300

    Create lab1.php

---
+++ lab1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+      <head>
+            <title>moya ctranica</title>
+            <link rel="stylsheet" href="stilek.css">
+            <link rel="shortcut icon" href="favicon.ico">
+      </head>
+      <body>
+           <div class="mydiv1">
+                 <?php
+                       echo "<h1>средняя диния с основаниями 12 и 18 равняется ", (12+18)/2, "</h1>"
+                 ?>
+           </div>
+       </body>
+</html>
